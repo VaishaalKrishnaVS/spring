1c10ece1812e602882f594ba61abbb01a0eb495a
VaishaalKrishnaVS <vaishaalinc@gmail.com>
2023-10-09 12:00:41 +0530

    initial commit

---
+++ bzulb/src/main/java/com/easybytes/bzulb/BzulbApplication.java
@@ -0,0 +1,13 @@
+package com.easybytes.bzulb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BzulbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BzulbApplication.class, args);
+    }
+
+}
---
+++ bzulb/src/test/java/com/easybytes/bzulb/BzulbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easybytes.bzulb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BzulbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
